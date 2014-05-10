f2a88661572c734579ea99109045e0798fe3acd5
a.harada <redhornet96@gmail.com>
2014-05-10 17:29:52 +0900

    modify

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get "/pages/:action" => "pages#:action"
 
-  #root 'welcome#index'
+  root 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
